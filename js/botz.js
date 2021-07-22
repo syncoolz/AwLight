4571d6cdf73a5ffc7eb50aa9e8f97eed01cca2d4
syncoolz <84556258+syncoolz@users.noreply.github.com>
2021-07-22 08:46:14 +0700

    Update botz.js
    fix bug Check mining

--- js/botz.js
+++ js/botz.js
@@ -73,52 +73,42 @@ class botz {
         timerMonitorRealtime.start()
     }
 
-    async CheckingMinings() {
-        const waitStatus = "Mining";
+    async CheckingMinings() {                
         botzz.countcheckstop = false;
         if (document.getElementById('AutoRestartFunction').checked == true) {
             const timeleft = document.getElementById('MinedOuttime').value
             const countCheckMining = parseFloat(timeleft)
-            if (waitStatus == document.getElementById("text-cooldown").innerHTML) {
-                /*    Swal.fire({
-                        icon: 'success',
-                        title: 'The system is mining',
-                        html: 'If you spend more than ' + + document.getElementById('MinedOuttime').value + ' seconds mining, the system will reset automatically.',
-                        showConfirmButton: false,
-                        timer: 5000
-                    }) */
-                bott.appendMessage(`Status : When mining takes too long ${timeleft} sec. (Auto reload)`)
-                const timerMiningCheck = new TaskTimer(1000);
-                timerMiningCheck.add([
-                    {
-                        id: 'MiningCheck',       // unique ID of the task
-                        //tickDelay: 1,       // 1 tick delay before first run
-                        tickInterval: countCheckMining,   // run every 10 ticks (10 x interval = 10000 ms)
-                        totalRuns: 1,       // run 2 times only. (set to 0 for unlimited times)
-                        callback(task) {
-                            console.log(`${task.id} task has run ${task.currentRuns} times.`);
-                            if (botzz.countcheckstop == false) {
-                                location.reload();
-                            } else {
-                                timerMiningCheck.stop()
-                            }
-                        }
-                    },
-                    {
-                        id: 'countMiningCheck',       // unique ID of the task
-                        tickInterval: 1,    // run every 5 ticks (5 x interval = 5000 ms)
-                        totalRuns: countCheckMining,      // run 10 times only. (set to 0 for unlimited times)
-                        callback(task) {
-                            //console.log(`${task.id} task has run ${count} times.`);
-                            document.getElementsByTagName('title')[0].text = `Mining.. ${Math.ceil(task.totalRuns - task.currentRuns)} Sec`
-                            if (botzz.countcheckstop == true) {
-                                timerMiningCheck.stop()
-                            }
+            bott.appendMessage(`Status : When mining takes too long ${timeleft} sec. (Auto reload)`)
+            const timerMiningCheck = new TaskTimer(1000);
+            timerMiningCheck.add([
+                {
+                    id: 'MiningCheck',       // unique ID of the task
+                    //tickDelay: 1,       // 1 tick delay before first run
+                    tickInterval: countCheckMining,   // run every 10 ticks (10 x interval = 10000 ms)
+                    totalRuns: 1,       // run 2 times only. (set to 0 for unlimited times)
+                    callback(task) {
+                        console.log(`${task.id} task has run ${task.currentRuns} times.`);
+                        if (botzz.countcheckstop == false) {
+                            location.reload();
+                        } else {
+                            timerMiningCheck.stop()
                         }
                     }
-                ]);
-                timerMiningCheck.start()
-            }
+                },
+                {
+                    id: 'countMiningCheck',       // unique ID of the task
+                    tickInterval: 1,    // run every 5 ticks (5 x interval = 5000 ms)
+                    totalRuns: countCheckMining,      // run 10 times only. (set to 0 for unlimited times)
+                    callback(task) {
+                        //console.log(`${task.id} task has run ${count} times.`);
+                        document.getElementsByTagName('title')[0].text = `Mining.. ${Math.ceil(task.totalRuns - task.currentRuns)} Sec`
+                        if (botzz.countcheckstop == true) {
+                            timerMiningCheck.stop()
+                        }
+                    }
+                }
+            ]);
+            timerMiningCheck.start()
         }
     }
     
